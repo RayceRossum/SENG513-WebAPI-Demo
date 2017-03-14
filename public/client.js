1ede16d36d2712b84b93de06d9f73ad604781855
Rayce Rossum <Rayce.Rossum@gmail.com>
2017-03-14 11:43:25 -0600

    Add image preview

--- public/client.js
+++ public/client.js
@@ -16,3 +16,20 @@ $('#fileUpload').submit(function(event) {
         }
     });
 });
+
+$('#file').change(function() {
+  readURL(this);
+});
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imagePreview').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
